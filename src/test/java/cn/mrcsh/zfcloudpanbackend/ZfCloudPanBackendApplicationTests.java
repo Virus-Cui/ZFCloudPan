a5d419411bbd98db8851d53aa0826149f7c55169
FeiFei <Virus_Cui@outlook.com>
2024-06-28 15:11:23 +0800

    初始化仓库

---
+++ src/test/java/cn/mrcsh/zfcloudpanbackend/ZfCloudPanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mrcsh.zfcloudpanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfCloudPanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
